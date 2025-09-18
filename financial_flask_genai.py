d4b17b7f6bffce88a61ec063327640bbe4a9e44a
subhajitgpt <subhagpa@gmail.com>
2025-09-18 11:18:30 +0400

    new code

--- financial_flask_genai.py
+++ financial_flask_genai.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 # --- API + Flask setup ---
-load_dotenv("C:\\Financial\\.env")
+load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY) if OPENAI_API_KEY else None
 
